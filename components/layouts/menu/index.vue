7833964aacb00c10f6555b00351be48590c4301a
Alex.V <spookinfish775@gmail.com>
2021-08-11 19:05:14 +0500

    tabletMenuAdaptive

--- components/layouts/menu/index.vue
+++ components/layouts/menu/index.vue
@@ -20,21 +20,59 @@
       </div>
       <span class="lang-bar link">{{ $t("menu.langBar") }}</span>
     </div>
+    <div class="menu-button" @click="openMenu">
+      <div class="top line"></div>
+      <div class="middle line"></div>
+      <div class="bottom line"></div>
+    </div>
+    <transition name="slide-fade">
+      <div class="menu-mobile" v-if="menuVisibility">
+        <MobileMenu />
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import ProductSubMenu from "./productsSubMenu.vue";
 import ContactSubMenu from "./contactsSubMenu.vue";
+import MobileMenu from "./mobileMenu/index.vue";
 export default {
+  data() {
+    return {
+      menuVisibility: false,
+    };
+  },
   components: {
     ProductSubMenu,
     ContactSubMenu,
+    MobileMenu,
+  },
+  methods: {
+    openMenu() {
+      if (this.menuVisibility == false) {
+        this.menuVisibility = true;
+      } else {
+        this.menuVisibility = false;
+      }
+      let btn = document.querySelector(".menu-button");
+      btn.classList.toggle("active");
+    },
   },
 };
 </script>
 
 <style lang="scss">
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(100%);
+}
 .menu-wrapper {
   width: 100%;
   background-color: transparent;
@@ -45,7 +83,6 @@ export default {
   justify-content: space-between;
   align-items: center;
   font-size: 10px;
-
   .link-group {
     width: 35%;
     height: 100%;
@@ -53,7 +90,6 @@ export default {
     justify-content: space-evenly;
     align-items: center;
     .link {
-      padding: 4.3em 0;
       font-size: 1.4em;
       font-weight: 700;
       text-transform: uppercase;
@@ -71,8 +107,93 @@ export default {
       right: 17.8em;
     }
   }
-  // @include max($large) {
-
-  // }
+  .logo {
+    padding-top: 2.9em;
+    width: 10.6em;
+    img {
+      width: 100%;
+    }
+  }
+  .menu-button {
+    display: none;
+  }
+  @include max($large) {
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 3.2em;
+    .link-group {
+      display: none;
+      .link {
+        font-size: 1.4em;
+      }
+      .productSubMenu {
+        display: none;
+        position: absolute;
+        top: 8.5em;
+        left: 17.8em;
+      }
+      .contactSubMenu {
+        display: none;
+        position: absolute;
+        top: 8.5em;
+        right: 17.8em;
+      }
+    }
+    .logo {
+      padding-top: 2.9em;
+      width: 10.6em;
+      img {
+        width: 100%;
+      }
+    }
+    .menu-button {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      width: 3.3em;
+      height: 2.93em;
+      z-index: 11;
+      .line {
+        height: 0.37em;
+        background-color: $main-color;
+        transition: 0.4s;
+      }
+      .top {
+        width: 2em;
+        align-self: flex-end;
+        margin-right: 0.37em;
+      }
+      .middle {
+        opacity: 1;
+      }
+      .bottom {
+        width: 2em;
+        margin-left: 0.37em;
+      }
+      &.active {
+        .line {
+          background-color: $default-text-color;
+        }
+        .middle {
+          opacity: 0;
+          // position: absolute;
+        }
+        .top {
+          transform: rotate(-47deg) translate(-2.1em);
+        }
+        .bottom {
+          transform: rotate(47deg) translate(-1.4em);
+        }
+      }
+    }
+    .menu-mobile {
+      width: 100vw;
+      height: 100vh;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
 }
 </style>
---
+++ components/layouts/menu/mobileMenu/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="mobile-menu-wrapper">
+    <div class="mobile-link-group">
+      <div class="link-wrapper">
+        <nuxt-link to="/" class="mobile-link">{{
+          $t("menu.productLink")
+        }}</nuxt-link>
+        <div class="arrow">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+      <div class="link-wrapper">
+        <nuxt-link to="/" class="mobile-link">{{
+          $t("menu.aboutLink")
+        }}</nuxt-link>
+      </div>
+      <div class="link-wrapper">
+        <nuxt-link to="/" class="mobile-link">{{
+          $t("menu.articlesLink")
+        }}</nuxt-link>
+      </div>
+      <div class="link-wrapper">
+        <nuxt-link to="/" class="mobile-link">{{
+          $t("menu.contactLink")
+        }}</nuxt-link>
+        <div class="arrow">
+          <div class="top"></div>
+          <div class="bottom"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.mobile-menu-wrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  width: 100vw;
+  height: 100vh;
+  background-color: $main-color;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  .mobile-link-group {
+    width: 100%;
+    height: 46.3em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-start;
+    .link-wrapper {
+      width: 92%;
+      font-size: 2em;
+      color: $default-text-color;
+      font-weight: 900;
+      padding-left: 12.4em;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      position: relative;
+      .mobile-link {
+        color: $default-text-color;
+      }
+      .arrow {
+        position: absolute;
+        right: 0;
+        .top {
+          width: 0.6em;
+          height: 0.15em;
+          background-color: $default-text-color;
+          transform: rotate(45deg) translateX(-3px);
+        }
+        .bottom {
+          width: 0.6em;
+          height: 0.15em;
+          background-color: $default-text-color;
+          transform: rotate(-45deg) translateX(-3px);
+        }
+      }
+    }
+  }
+}
+</style>
