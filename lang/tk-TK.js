41014f9dc90695f51c28ce698e6a3c05c8382c06
Alex.V <spookinfish775@gmail.com>
2021-08-15 20:57:40 +0500

    translate

--- lang/tk-TK.js
+++ lang/tk-TK.js
@@ -1,3 +1,44 @@
 export default {
-
+    menu: {
+        productLink: "Önümlerimiz",
+        aboutLink: "Biz barada",
+        articlesLink: "Makalalar",
+        contactLink: "Salgymyz",
+        socialLink: "Biz sosial ulgamlarda",
+        emailLabel: "kompaniýanyň täzeliklerine we aksiýalaryna ýazylmak",
+        rightsLabel: "© 2021 “Bagtly zamana” HJ. Ähli hukuklar goragly.",
+        langBar: "RU",
+        productSubMenuLinks: [
+            "Ak nur kir ýuwujy serişdesi",
+            "Aý nur kir ýuwujy serişdesi",
+            "Päkize gözellik sabyny",
+            "Kislorodly agardyjysy",
+        ],
+        contactsSubMenuLink: "Diler bolmaklyk",
+        contactsSubMenuLocation: "Türkmenistan, Ahal welaýaty, Ak bugdaý etraby Änew şäheri, Telekeçiler köçesi, 14-nji jaý."
+    },
+    form: {
+        namePlaceholder: "Ady",
+        surnamePlaceholder: "Familiýasy",
+        emailPlaceholder: "E-mail",
+        numberPlaceholder: "Telefon belgiňiz",
+        countryPlaceholder: "Ýurt",
+        cityPlaceholder: "Şäher",
+        textPlaceholder: "Tekst",
+        agreePlaceholder: "Men öz şahsy maglumatlarymyň işlenmegine razy",
+        sendPlaceholder: "Ugratmak"
+    },
+    dealer: {
+        title: "Diler bolmaklyk",
+        description: "Diler bolmaklyk üçin aşakdaky beýany dolduryp bize iberiň.",
+        fullNamePlaceholder: "F.A.A a",
+        companyPlaceholder: "Kompaniýa",
+        emailPlaceholder: "E-mail",
+        numberPlaceholder: "Telefon belgiňiz",
+        countryPlaceholder: "Ýurt",
+        cityPlaceholder: "Şäher",
+        textPlaceholder: "Tekst",
+        agreePlaceholder: "Men öz şahsy maglumatlarymyň işlenmegine razy",
+        submitPlaceholder: "Ugratmak",
+    },
 }
